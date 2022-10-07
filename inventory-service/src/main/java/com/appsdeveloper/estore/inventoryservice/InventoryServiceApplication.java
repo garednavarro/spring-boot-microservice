3ac93a19d97412f71f449d2b852b468f95eb28f6
Mario Navarro <menavarroc@protonmail.com>
2022-10-07 12:21:19 -0500

    command runner methos removed from main class on inventory service

--- inventory-service/src/main/java/com/appsdeveloper/estore/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/appsdeveloper/estore/inventoryservice/InventoryServiceApplication.java
@@ -15,20 +15,5 @@ public class InventoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
-    @Bean
-    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
-        return args -> {
-            InventoryEntity inventory = new InventoryEntity();
-            inventory.setSkuCode("iphone_13");
-            inventory.setQuantity(100);
-
-            InventoryEntity inventory1 = new InventoryEntity();
-            inventory1.setSkuCode("iphone_13_red");
-            inventory1.setQuantity(0);
-
-            inventoryRepository.save(inventory);
-            inventoryRepository.save(inventory1);
-        };
-    }
 
 }
